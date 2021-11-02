eef66ef83bdcb93b2f6d2ba6d97742dabd3f0f6d
wangpingan <pingan8787@qq.com>
2021-11-03 00:04:20 +0800

    v0.1.3 支持生成选中当前文本，作为当前链接的md格式内容

--- background.js
+++ background.js
@@ -19,3 +19,37 @@ chrome.runtime.onInstalled.addListener(() => {
         console.log(`插件加载完成，当前模式：${curMode}`);
     })
 });
+
+chrome.contextMenus.create({
+    id: 'ToyReader',
+    title: "生成分享链接: %s",
+    contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
+});
+
+chrome.contextMenus.onClicked.addListener(async function(info, tab) {
+    const { selectionText, pageUrl } = info;
+    const copyText = `[${selectionText}](${pageUrl})`;
+    console.log('【ToyReader 复制内容】', info, copyText)
+    try {
+        await navigator.clipboard.writeText(copyText);
+    } catch (error) {
+        function copyFun (args) {
+            const { copyText } = args;
+            console.log('[ToyReader 复制操作]', copyText)
+            const n = document.createElement("textarea");
+            n.textContent = copyText;
+            document.body.appendChild(n);
+            n.select();
+            document.execCommand("Copy", false);
+        }
+
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: copyFun,
+            args: [{copyText}]
+        });
+    }
+    // 注意不能使用location.href，因为location是属于background的window对象
+    // chrome.tabs.create({url: 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURI(params.selectionText)});
+});
